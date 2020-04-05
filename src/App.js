83b89876891d74cb233393677f2011f782794f47
samjgorman <samjgorman@gmail.com>
2020-04-05 00:10:49 -0700

    fix broken redirect

--- src/App.js
+++ src/App.js
@@ -55,12 +55,12 @@ class App extends React.Component {
     this.onCountySelectionMobileMenu = (selected_county) => {
       document.getElementsByClassName("County-info")[0].scrollTop = 0;
       this.onCountySelectionMobileMenu = (selected_county) => {
-              document.getElementsByClassName("County-info")[0].scrollTop = 0;
+        //       document.getElementsByClassName("County-info")[0].scrollTop = 0;
         
               this.setState({
                 mobile_info_county: selected_county,
-                active_mobile_component: "news", //by default, other possible values are "map", "menu"
-                prev_active_mobile_component: "news"
+        //         active_mobile_component: "news", //by default, other possible values are "map", "menu"
+        //         prev_active_mobile_component: "news"
               })
             }
       // this.setState({
